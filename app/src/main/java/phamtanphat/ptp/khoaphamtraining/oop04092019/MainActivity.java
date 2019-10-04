5bcd49bacc3037f0a9564868310a58ba24f63e07
phatdroid94 <phatdroid94@gmail.com>
2019-10-04 21:12:19 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/oop04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/oop04092019/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -35,8 +36,32 @@ public class MainActivity extends AppCompatActivity{
         // 2 : Tinh thua ke : extend , implements
         // tai sao su dung ke thua
         // Dua con co kieu du giong cha
-        Giangvien phat = new Giangvien();
-        phat.luatuoi();
+//        Giangvien phat = new Giangvien();
+//        phat.luatuoi();
 
+        // Overload : Nap chong
+        // Override : ghi de
+
+
+        // Dac diem : Giup cho phuong thuc thực được nhiều chức năng
+        // Nhược điểm : Quá dài
+
+        // Pham vi tạo ra: các phương thức nạp chồng sẽ ở cùng 1 nơi
+
+        // Các phương thức nạp chồng sẽ giống nhau về tên , khác nhau
+//        về tham số truyền vào
+
+        tinh(10 , 5);
+    }
+    // method : tinh
+    // dien tich tam giac : a * h / 2
+    // chu vi tam giac : a + b + c
+    private void tinh(int canhday , int chieucao){
+        int ketqua = canhday * chieucao / 2;
+        Toast.makeText(this, "Diện tích tam giác " +ketqua, Toast.LENGTH_SHORT).show();
+    }
+    private void tinh(int canhday1 , int canhday2 , int canhday3){
+        int ketqua = canhday1 + canhday2 + canhday3;
+        Toast.makeText(this, "Chu vi tam giác " +ketqua , Toast.LENGTH_SHORT).show();
     }
 }
